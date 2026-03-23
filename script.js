590635935e68f1bb3174b613bacbecf96aa11e73
vincentshalom <vincentshalom7@gmail.com>
2026-03-23 04:23:30 +0100

    fix cart menu

--- script.js
+++ script.js
@@ -3,18 +3,22 @@ const menuPanel = document.getElementById("menuPanel");
 const openBtn = document.getElementById("openMenu");
 const closeBtn = document.getElementById("closeMenu");
 
-/* OPEN MENU FUNCTION */
+const cart = document.getElementById("cart");
+const cartPanel = document.getElementById("cartPanel");
+const openCart = document.getElementById("openCart");
+const closeCart = document.getElementById("closeCart");
+const overlay = document.getElementById("cartOverlay");
+
+// MOBILE MENU SECTION STARTS
 function openMenu() {
   mobileMenu.classList.remove("opacity-0", "pointer-events-none");
   menuPanel.classList.remove("-translate-x-full");
 }
 
-/* CLOSE MENU FUNCTION */
 function closeMenu() {
   mobileMenu.classList.add("opacity-0");
   menuPanel.classList.add("-translate-x-full");
 
-  /* delay hiding clicks until animation finishes */
   setTimeout(() => {
     mobileMenu.classList.add("pointer-events-none");
   }, 300);
@@ -23,7 +27,27 @@ function closeMenu() {
 openBtn.addEventListener("click", openMenu);
 closeBtn.addEventListener("click", closeMenu);
 
-// close when clicking outside
 mobileMenu.addEventListener("click", (e) => {
   if (e.target === mobileMenu) closeMenu();
 });
+
+//MOBILE MENU SECTION ENDS
+
+//CART SECTION STARTS
+function openCartFunc() {
+  cart.classList.remove("hidden");
+  setTimeout(() => {
+    cartPanel.classList.remove("translate-x-full");
+  }, 10);
+}
+
+function closeCartFunc() {
+  cartPanel.classList.add("translate-x-full");
+  setTimeout(() => {
+    cart.classList.add("hidden");
+  }, 300);
+}
+
+openCart.addEventListener("click", openCartFunc);
+closeCart.addEventListener("click", closeCartFunc);
+overlay.addEventListener("click", closeCartFunc);
